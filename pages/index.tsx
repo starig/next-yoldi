5f82c2a022771d92166d1888d56777d419bb30a1
starig <starkovivan3@gmail.com>
2023-03-22 15:12:05 +0800

    Merge remote-tracking branch 'origin/main'

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,7 +42,6 @@ export default function Home() {
         );
 
     if (data) {
-        console.log(data)
         return (
             <div className={styles.home}>
                 <div className={styles.container}>
